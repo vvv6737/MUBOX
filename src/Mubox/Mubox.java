3972146c0c1019b9d033d43bd2bc5e35d9602641
jeonghoon <vvv6737@naver.com>
2021-02-20 01:30:34 +0900

    업데이트!

--- src/Mubox/Mubox.java
+++ src/Mubox/Mubox.java
@@ -1,27 +1,93 @@
 package Mubox;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.PaintContext;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Mubox extends JFrame {
 	
 	private Image screenImage;
 	private Graphics screenGraphic;
 	
-	private Image background;
+	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../Images/exitButtonEntered.png"));
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../Images/exitButtonBasic.png"));
+	
+	private Image background = new ImageIcon(Main.class.getResource("../Images/background.jpg")).getImage();
+	private JLabel menubar = new JLabel(new ImageIcon(Main.class.getResource("../Images/menuBar.png")));
+	
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	
+	private int mouseX, mouseY; //메뉴바를 잡고 흔들수있다.
 	
 	public Mubox() {
+		setUndecorated(true);
 		setTitle("MUBOX");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0)); //paintComponents을 했을때 배경이 하얀색으로 바뀜
+		setLayout(null); //버튼이나 JLabel을 넣었을 때 그 위치 그대로 
 		
-		background = new ImageIcon(Main.class.getResource("../Images/background.jpg")).getImage();
+		menubar.setBounds(0, 0, 1240, 30); //메뉴바 위치와 크기을 정해주는것
+		menubar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menubar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);
+			}
+		});
+		add(menubar);
+		
+		exitButton.setBounds(1245, 0, 30, 30);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnteredImage); //마우스가 올라갈때 이미지가 entered로 바뀐다
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); //마우스를 올리면 커서가 손가락모양이 된다
+				Music buttonEnterdMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnterdMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonBasicImage); //마우스에서 나올때 원래 색으로 돌아간다
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); //손가락 모양의 커서를 되돌린다.
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnterdMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnterdMusic.start();
+				try {
+					Thread.sleep(1000); //소리가 나온다음 정상적으로 
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0); //종료
+			}
+		});
+		add(exitButton);
 		
 		Music intromusic = new Music("intromusic.mp3", true);
 		intromusic.start();
@@ -34,6 +100,7 @@ public class Mubox extends JFrame {
 	}
 	private void screenDraw(Graphics g) {
 		g.drawImage(background, 0, 0, null);
+		paintComponents(g);
 		this.repaint();
 	}
 }
