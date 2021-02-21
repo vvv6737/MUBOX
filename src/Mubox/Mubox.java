df5e6195a996603de676de9986e512799e34df73
jeonghoon <vvv6737@naver.com>
2021-02-21 19:25:35 +0900

    게임시작화면, 이미지 추가

--- src/Mubox/Mubox.java
+++ src/Mubox/Mubox.java
@@ -26,14 +26,25 @@ public class Mubox extends JFrame {
 	private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../Images/quitButtonEntered.png"));
 	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../Images/quitButtonBasic.png"));
 	
+	private ImageIcon leftButtonEnteredImage = new ImageIcon(Main.class.getResource("../Images/leftButtonEntered.png"));
+	private ImageIcon leftButtonBasicImage = new ImageIcon(Main.class.getResource("../Images/leftButtonBasic.png"));
+	private ImageIcon rightButtonEnteredImage = new ImageIcon(Main.class.getResource("../Images/rightButtonEntered.png"));
+	private ImageIcon rightButtonBasicImage = new ImageIcon(Main.class.getResource("../Images/rightButtonBasic.png"));
+	
+	private Image titleImage = new ImageIcon(Main.class.getResource("../Images/Erase You title image.png")).getImage();
+	private Image selectedImage = new ImageIcon(Main.class.getResource("../Images/CHANGMO - Erase You.png")).getImage();
 	private Image background = new ImageIcon(Main.class.getResource("../Images/background.jpg")).getImage();
 	private JLabel menubar = new JLabel(new ImageIcon(Main.class.getResource("../Images/menuBar.png")));
 	
 	private JButton exitButton = new JButton(exitButtonBasicImage);
 	private JButton startButton = new JButton(startButtonBasicImage);
 	private JButton quitButton = new JButton(quitButtonBasicImage);
+	private JButton leftButton = new JButton(leftButtonBasicImage);
+	private JButton rightButton = new JButton(rightButtonBasicImage);
 	
 	private int mouseX, mouseY; //메뉴바를 잡고 흔들수있다.
+	
+	private boolean isMainScreen = false;
 	
 	public Mubox() {
 		setUndecorated(true);
@@ -119,10 +130,67 @@ public class Mubox extends JFrame {
 				// 게임 시작 이벤트
 				startButton.setVisible(false);
 				quitButton.setVisible(false);
+				leftButton.setVisible(true);
+				rightButton.setVisible(true);
 				background = new ImageIcon(Main.class.getResource("../Images/mainBackground.jpg")).getImage();
+				isMainScreen = true;
 			}
 		});
 		add(startButton);
+		
+		leftButton.setVisible(false);
+		leftButton.setBounds(140, 310, 64, 64);
+		leftButton.setBorderPainted(false);
+		leftButton.setContentAreaFilled(false);
+		leftButton.setFocusPainted(false);
+		leftButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				leftButton.setIcon(leftButtonEnteredImage); //마우스가 올라갈때 이미지가 entered로 바뀐다
+				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); //마우스를 올리면 커서가 손가락모양이 된다
+				Music buttonEnterdMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnterdMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				leftButton.setIcon(leftButtonBasicImage); //마우스에서 나올때 원래 색으로 돌아간다
+				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); //손가락 모양의 커서를 되돌린다.
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnterdMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnterdMusic.start();
+				// 왼쪽 버튼 이벤트
+			}
+		});
+		add(leftButton);
+		
+		rightButton.setVisible(false);
+		rightButton.setBounds(1080, 310, 64, 64);
+		rightButton.setBorderPainted(false);
+		rightButton.setContentAreaFilled(false);
+		rightButton.setFocusPainted(false);
+		rightButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rightButton.setIcon(rightButtonEnteredImage); //마우스가 올라갈때 이미지가 entered로 바뀐다
+				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); //마우스를 올리면 커서가 손가락모양이 된다
+				Music buttonEnterdMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnterdMusic.start();
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rightButton.setIcon(rightButtonBasicImage); //마우스에서 나올때 원래 색으로 돌아간다
+				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR)); //손가락 모양의 커서를 되돌린다.
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Music buttonEnterdMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonEnterdMusic.start();
+				// 오른쪽 버튼 이벤트
+			}
+		});
+		add(rightButton);
 		
 		quitButton.setBounds(900, 520, 270, 100);
 		quitButton.setBorderPainted(false);
@@ -155,17 +223,25 @@ public class Mubox extends JFrame {
 		});
 		add(quitButton);
 		
+		
 		Music intromusic = new Music("intromusic.mp3", true);
 		intromusic.start(); //인트로 뮤직 스타트
 	}
+	
 	public void paint(Graphics g) {
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(screenImage, 0, 0, null);
 	}
+	
 	private void screenDraw(Graphics g) {
 		g.drawImage(background, 0, 0, null);
+		if(isMainScreen) {
+			g.drawImage(selectedImage, 400, 100, null);
+			g.drawImage(titleImage, 340, 70, null);
+		}
+			
 		paintComponents(g);
 		this.repaint();
 	}
